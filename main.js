6d2565ccde61fef8783b0651bbf9b5d5c03fa1b8
Ruben Valseca <rubn.vp@gmail.com>
2018-03-20 05:49:23 +0100

    remove some complex methods

--- main.js
+++ main.js
@@ -10,46 +10,6 @@ var pokemonTypeColor = {
     "electric": "#F8D030"
 };
 
-Vue.component('nice-input', {
-    template: `
-<div class="nice-input-wrapper">
-    <input class="nice-input" type="text" placeholder="Search by name" :value="value" @input="changeText">
-    <span class="focus-border"><i></i></span>
-</div>`,
-    props: ['value'],
-    methods: {
-        changeText: function(event){
-            this.$emit('input', event.target.value);
-        },
-    },
-});
-
-
-Vue.component('nice-checkbox', {
-    template: '#nice-checkbox-template',
-    props: ['value', 'color', 'name', 'count'],
-    computed: {
-        selectedList: {
-            get: function() {
-                return this.value;
-            },
-            set: function(value) {
-                this.$emit('input', value);
-            },
-        },
-    },
-});
-
-var PokemonCard = {
-    template: '#pokemon-card-template',
-    props: ['pokemon', 'colors'],
-    methods: {
-        remove: function() {
-            this.$emit('remove', this.pokemon);
-        },
-    },
-};
-
 var app = new Vue({
     el: '#app',
     data: {
@@ -71,27 +31,10 @@ var app = new Vue({
                         pokemon.types[1] && that.selectedPokemonTypes.includes(pokemon.types[1]);
                 });
         },
-        pokemonCountByType: function() {
-            var pokemonCountByType = {};
-            for (type in pokemonTypeColor) {
-                pokemonCountByType[type] = this.filteredPokemons
-                    .filter(function(pokemon){ // filter by type
-                        return pokemon.types.includes(type);
-                    })
-                    .length;
-            }
-            return pokemonCountByType;
-        }
     },
     methods: {
         getPokemonTypeColor: function(type) {
             return pokemonTypeColor[type];
-        },
-        getBackgroundColors: function(types) {
-            return types
-                .map(function(type){
-                    return pokemonTypeColor[type];
-                });
         },
         removePokemon: function(pokemonToRemove) {
             this.pokemons = this.pokemons
@@ -109,9 +52,6 @@ var app = new Vue({
             .then(function(pokemons){
                 that.pokemons = pokemons;
             });
-    },
-    components: {
-        PokemonCard: PokemonCard,
     },
 });
 
