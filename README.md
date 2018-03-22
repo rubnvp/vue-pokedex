ac2e7edfcba41b05f6508c648d0e44011238261b
Ruben Valseca <rubn.vp@gmail.com>
2018-03-22 11:43:54 +0100

    added screenshot demo

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 Just open index.html and it will work :)
 
+![screensot demo](images/screenshot-demo.png "Screenshot Demo")
+
+### ES6 version
+
 If you know git, checkout to the branch 'es6' for a cleaner ES6 version (but could not work in all browsers)
--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-(function(){
+(function(window){
 // Vue "normal JS" (ES5) example of use, more info about ES5 and ES6 https://medium.com/front-end-hacking/es6-vs-es5-9254f8390332
 // in the git branch 'es6' you can find the ES6 version (is highly support in some browsers but check first at https://caniuse.com/)
 
-// this register the component to be global available in every Vue template, usually I prefer to define this as
+// Vue.component register the component to be global available in every Vue template, usually I prefer to define this as
 // an object (the second parameter) an include it in every Vue instance that I need (explicit better than implicit)
-// https://vuejs.org/v2/guide/components.html
+// visit https://vuejs.org/v2/guide/components.html for more info
 Vue.component('pokemon-card', {
     props: ['pokemon', 'typeColor'],
     template: '#pokemon-card-template', // or we can just put the html here in a string, or even JSX (build step needed)
@@ -17,9 +17,7 @@ Vue.component('pokemon-card', {
 
 var app = new Vue({
     el: '#app',
-    // usually you define data as a function that returns an object
-    // in order to avoid have the same data if you have more than one instance
-    data: {
+    data: { // usually you define data as a function that returns an object in order to avoid have the same data if you have more than one instance
         searchText: '',
         TYPE_COLOR: {
             "grass": "#78C850",
@@ -46,7 +44,7 @@ var app = new Vue({
                 .filter(function(pokemon) { // filter by pokemon type (checkboxes)
                     if (that.selectedPokemonTypes.length === 0) return true; // no checked checkbox means no filter
                     return pokemon.types
-                        .find(function(type){ // for each pokemon type we try to find if it's included in the array selectedPokemonTypes 
+                        .find(function(type){ // for each pokemon type we try to find if it's included in the array of selectedPokemonTypes 
                             return that.selectedPokemonTypes.includes(type);
                         });
                 });;
@@ -63,7 +61,7 @@ var app = new Vue({
     created: function(){ // function executed just after setting the reactivity https://vuejs.org/v2/guide/instance.html#Lifecycle-Diagram
         var that = this;
         // fetch native api, https://caniuse.com/#feat=fetch
-        // you also have the pokemon json info in data/pokemons.json but you will need a simple local server to serve it
+        // you also have the pokemon json info in data/pokemons.json but you will need a local server to serve it
         fetch('https://api.jsonbin.io/b/5ab37f77989617146bd6eb29') 
             .then(function(response){
                 return response.json();
@@ -86,4 +84,4 @@ var app = new Vue({
     });*/
 
 window.app = app; // just put the Vue instance available in the JS console (don't do this in production)
-})();
+})(window); // this is a self invoke function https://sarfraznawaz.wordpress.com/2012/01/26/javascript-self-invoking-functions/
